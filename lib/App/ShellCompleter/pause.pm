004bc789af3e29f949c61bbc17af9c2d814dcc49
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2017-01-13 16:03:25 +0700

    Initial commit

---
+++ lib/App/ShellCompleter/pause.pm
@@ -0,0 +1,16 @@
+package App::ShellCompleter::pause;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Improved shell completion for pause
+
+=head1 SYNOPSIS
+
+See L<_pause> included in this distribution.
+
+
+=head1 SEE ALSO
+
+L<pause> from L<App::pause>
